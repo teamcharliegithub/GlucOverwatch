679c9de7261821e97fcbab1fe715238ea2b3f62a
teamcharliegithub <55711242+teamcharliegithub@users.noreply.github.com>
2022-04-09 13:02:57 -0400

    Update glucoverwatch.py
    Added control loops

--- glucoverwatch.py
+++ glucoverwatch.py
@@ -5,3 +5,17 @@ pwd = ""  # Password omitted in repository
 dexcom = Dexcom(username, pwd)
 
 bg = dexcom.get_current_glucose_reading()
+trend = bg.trend
+
+while True:
+    if (bg <= 70):
+        # Twilio Code Here
+    elif (bg <= 120 and trend == 7):
+        # Twilio Code Here
+    elif (bg <= 100 and (trend == 6 or trend == 7):
+          # Twilio Code Here
+    else:
+    time.sleep(360)
+    bg = dexcom.getcurrent_glucose_reading()
+    trend = bg.trend
+
