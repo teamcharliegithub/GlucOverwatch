8896243735649c68c2abf8f2252c111b1b0e01fe
teamcharliegithub <55711242+teamcharliegithub@users.noreply.github.com>
2022-04-09 13:11:50 -0400

    Update glucoverwatch.py

--- glucoverwatch.py
+++ glucoverwatch.py
@@ -1,21 +1,28 @@
+
 from pydexcom import Dexcom
 import time
 username = "brmotta"
 pwd = ""  # Password omitted in repository
 dexcom = Dexcom(username, pwd)
 
-bg = dexcom.get_current_glucose_reading()
-trend = bg.trend
+def update():
+    bg = dexcom.get_current_glucose_reading()
+    value = bg.value
+    trend = bg.trend
+    return bg, value, trend
+bg, value, trend = update()
 
 while True:
     if (bg <= 70):
         # Twilio Code Here
+        pass
     elif (bg <= 120 and trend == 7):
         # Twilio Code Here
-    elif (bg <= 100 and (trend == 6 or trend == 7):
+        pass
+    elif (bg <= 100 and (trend == 6 or trend == 7)):
           # Twilio Code Here
+          pass
     else:
+        pass
     time.sleep(360)
-    bg = dexcom.getcurrent_glucose_reading()
-    trend = bg.trend
-
+    bg, value, trend = update()
